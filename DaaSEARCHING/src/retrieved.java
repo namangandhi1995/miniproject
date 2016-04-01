1b5ded516aa8ab4790c3ea361c70daa9c0c90a26
aakarshgoel96 <aakarshgoel96@gmail.com>
2016-04-02 02:04:42 +0530

    deleted test.java and applied onclick feature on retrieved files @namangandhi1995 solve onclick issue

--- DaaSEARCHING/src/retrieved.java
+++ DaaSEARCHING/src/retrieved.java
@@ -1,4 +1,10 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import java.awt.Color;
+import java.awt.Desktop;
 import java.awt.Font;
 import java.io.File;
 
@@ -6,9 +12,9 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
 
-public class retrieved extends JFrame {
+public class retrieved extends JFrame  {
 	 JLabel l1;
-  retrieved(String s1)
+  retrieved( final String s1)
   {
       
         setSize(1000, 1000);
@@ -24,16 +30,18 @@ public class retrieved extends JFrame {
        l1.setBounds(100, 30, 400, 30);  
        add(l1);
      
-       int n,y;
+       int n,y,c=0;
        n=100;
        y=100;
       
        File dir= new File(s1);
-		for(File f: dir.listFiles()){
-			if(f.getName().contains(".pdf")){
+		for( File f: dir.listFiles()){
+			if(f.getName().contains(".pdf")|| f.getName().contains(".txt")|| f.getName().contains(".doc")||f.getName().contains(".docx")){
 				
 				 l1 = new JLabel(f.getName());
-
+				final String x;
+				  x=f.getName();
+				  System.out.println(s1+"/"+x);
 			       l1.setForeground(Color.red);
 
 			       l1.setFont(new Font("Serif", Font.BOLD, 20));
@@ -43,10 +51,26 @@ public class retrieved extends JFrame {
 			       add(l1);
 			       n=n+50;
 			       y=y+50;
+			     
+				l1.addMouseListener(new MouseAdapter() {
+			            public void mousePressed(MouseEvent me){
+			           	
+			            if (Desktop.isDesktopSupported()) {
+			                try {
+			                    File myFile = new File( s1+"/"+x);
+			                 System.out.println(s1+"/"+x);
+			                    Desktop.getDesktop().open(myFile);
+			                } catch (Exception e) {
+			                    // no application registered for PDFs
+			                }
+			            }
+			            }
+			            });
+			            }
 			}
 		}
-	
-       }
+			
+       
 
 	/**
 	 * @param args
